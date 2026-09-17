4eff2f782c73648911e36d119ff2c846806b5073
Mariana Paredes <mariana.andrea.paredes@gmail.com>
2026-09-17 14:20:17 -0300

    editando Timeline y Expertise

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -16,50 +16,50 @@ function Timeline() {
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
             contentArrowStyle={{ borderRight: '7px solid  white' }}
-            date="2022 - present"
+            date="2026 - present"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Technology Consultant</h3>
-            <h4 className="vertical-timeline-element-subtitle">Dallas, TX</h4>
+            <h3 className="vertical-timeline-element-title">Backend Developer</h3>
+            <h4 className="vertical-timeline-element-subtitle">Asap Consulting, Argentina</h4>
             <p>
-              Full-stack Web Development, GenAI/LLM, Project Management, Business Development
+              Backend Development, PHP, API REST, Microservices, Postgres.
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 - 2022"
+            date="2025 - 2026"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Full Stack Engineer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <h3 className="vertical-timeline-element-title">Wordpress Developer</h3>
+            <h4 className="vertical-timeline-element-subtitle">MTS, Argentina</h4>
             <p>
-              Frontend Development, Backend Development, User Experience, Team Leading
+              Backend Development, User Experience, Scrum Master
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2021 - 2021"
+            date="2025"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Staff Engineer Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <h3 className="vertical-timeline-element-title">Backend Developer</h3>
+            <h4 className="vertical-timeline-element-subtitle">Freelance</h4>
             <p>
-              Full-stack Development, API Development, User Experience
+              Node.js, Nest.js, Express, MySQL, Prisma, Docker, Git.
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 - 2020"
+            date="2019-2024"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Data Analyst Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Tokyo, Japan</h4>
+            <h3 className="vertical-timeline-element-title">Fullstack Developer</h3>
+            <h4 className="vertical-timeline-element-subtitle">Dicsys, Argentina</h4>
             <p>
-              Automation, Data Governance, Statistical Analysis
+              Fullstack Development, PHP, Node.js, API REST, Express, Mysql
             </p>
           </VerticalTimelineElement>
         </VerticalTimeline>
